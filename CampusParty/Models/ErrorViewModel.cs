ee756c2bc3dac4aba27f4e092c01ba14569ef28b
JuanJosePalomino <87874779+JuanJosePalomino@users.noreply.github.com>
2022-10-30 13:20:58 -0500

    Agregar archivos de proyecto.

---
+++ CampusParty/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace CampusParty.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
